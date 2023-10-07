19d344a9a42ffeb3273dae1ee13946b6df5ca6f2
stron <yaneliyahu83@gmail.com>
2023-10-07 18:45:03 +0300

    Add step-title-content constant file

---
+++ src/features/step-title-content.ts
@@ -0,0 +1,14 @@
+export const STEP_ONE_TITLE = "Personal info";
+export const STEP_TWO_TITLE = "Select your plan";
+export const STEP_THREE_TITLE = "Pick add-ons ";
+export const STEP_FOUR_TITLE = "Finishing up";
+
+export const STEP_ONE_SUB_TITLE = "Please provide your name, email address, and phone number.";
+export const STEP_TWO_SUB_TITLE = "You have the option of monthly or yearly billing.";
+export const STEP_THIRD_SUB_TITLE = "Add-ons help enhance your gaming experience.";
+export const STEP_FOUR_SUB_TITLE = "Double-check everything looks OK before confirming.";
+
+export const STEPPER_ONE_TITLE = "Your info";
+export const STEPPER_TWO_TITLE = "Select plan";
+export const STEPPER_THREE_TITLE = "Add-ons";
+export const STEPPER_FOUR_TITLE = "Summary";
