9919ccc9cf242f4834c49ac18b4c719000228666
stron <yaneliyahu83@gmail.com>
2023-10-25 20:13:27 +0300

    Create custom hook that holds all form data states

---
+++ src/hooks/use-form-data.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Steps, Billing, Plan, Addon } from "../shared.types";
+
+export const useFormData = () => {
+  const [activeStep, setActiveStep] = useState<Steps>(4);
+  const [planBilling, setPlanBilling] = useState<Billing>("Monthly");
+  const [selectedPlan, setSelectedPlan] = useState<Plan | null>(null);
+  const [selectedAddons, setSelectedAddons] = useState<Addon[]>([]);
+  const [isConfirmed, setIsConfirmed] = useState<boolean>(false);
+  const CHOOSE_PLAN_STEP = 2;
+
+  const formSummaryChangeHandler = (): void => {
+    setPlanBilling("Monthly");
+    setSelectedPlan(null);
+    setSelectedAddons([]);
+    setActiveStep(CHOOSE_PLAN_STEP);
+  };
+
+  return {
+    activeStep,
+    planBilling,
+    selectedPlan,
+    selectedAddons,
+    isConfirmed,
+    setActiveStep,
+    setPlanBilling,
+    setSelectedPlan,
+    setSelectedAddons,
+    formSummaryChangeHandler,
+    setIsConfirmed,
+  };
+};
